06cd2c77a75215106e3dea30ced8c3c8caea83a9
Grant Nelson <grantnelson1@gmail.com>
2017-11-19 11:53:34 -0700

    Added a generic interface for diff-ing variant slices.

--- diff.go
+++ diff.go
@@ -2,50 +2,89 @@ package diff
 
 import "strings"
 
-type stepType int
+// StepType is the steps of the levenshtein path.
+type StepType int
 
 const (
-	equalStep stepType = iota
-	diffStep
-	addedStep
-	removedStep
+	EqualStep   StepType = iota // EqualStep indicates A and B entries are equal
+	AddedStep                   // AddStep indicates B was added
+	RemovedStep                 // RemoveStep indicates A was removed
 )
 
-// PartDiff gets the labelled differences between the values.
+// Comparable is a simple interface for generic difference determination.
+type Comparable interface {
+
+	// ALength is the length of the first list being compared.
+	ALength() int
+
+	// BLength is the length of the secton list being compared.
+	BLength() int
+
+	// Equals determines if the entries in the two given indices are equal.
+	Equals(aIndex, bIndex int) bool
+}
+
+// stringSliceComparable is a comparable for two string slices.
+type stringSliceComparable struct {
+	a []string
+	b []string
+}
+
+func (comp *stringSliceComparable) ALength() int { return len(comp.a) }
+func (comp *stringSliceComparable) BLength() int { return len(comp.b) }
+func (comp *stringSliceComparable) Equals(aIndex, bIndex int) bool {
+	return comp.a[aIndex] == comp.b[bIndex]
+}
+
+// Strings gets the labelled differences between the values.
 // The given seperator will split the values and join the result.
-func PartDiff(a, b, sep string) string {
+func Strings(a, b, sep string) string {
 	aParts := strings.Split(a, sep)
 	bParts := strings.Split(b, sep)
-	result := Diff(aParts, bParts)
+	result := Slices(aParts, bParts)
 	return strings.Join(result, sep)
 }
 
-// Diff gets the labelled difference between the two slices.
-func Diff(a, b []string) []string {
+// Slices gets the labelled difference between the two slices.
+func Slices(a, b []string) []string {
 	result := []string{}
-	aRun, bRun := []string{}, []string{}
 	aIndex, bIndex := 0, 0
-	_, path := levenshteinDistance(a, b, len(a), len(b))
+	path := Path(&stringSliceComparable{a: a, b: b})
 	for _, step := range path {
 		switch step {
-		case equalStep:
-			result = append(result, aRun...)
-			result = append(result, bRun...)
-			aRun, bRun = []string{}, []string{}
+		case EqualStep:
 			result = append(result, " "+a[aIndex])
 			aIndex++
 			bIndex++
-		case diffStep:
-			aRun = append(aRun, "-"+a[aIndex])
-			bRun = append(bRun, "+"+b[bIndex])
-			aIndex++
+		case AddedStep:
+			result = append(result, "+"+b[bIndex])
 			bIndex++
-		case addedStep:
-			bRun = append(bRun, "+"+b[bIndex])
-			bIndex++
-		case removedStep:
-			aRun = append(aRun, "-"+a[aIndex])
+		case RemovedStep:
+			result = append(result, "-"+a[aIndex])
 			aIndex++
+		}
+	}
+	return result
+}
+
+// Path gets the difference path for the two given items.
+func Path(comp Comparable) []StepType {
+	result := []StepType{}
+	_, path := levenshteinDistance(comp, comp.ALength(), comp.BLength())
+
+	// Group additions and removals together.
+	aRun, bRun := []StepType{}, []StepType{}
+	for _, step := range path {
+		switch step {
+		case EqualStep:
+			result = append(result, aRun...)
+			result = append(result, bRun...)
+			aRun, bRun = []StepType{}, []StepType{}
+			result = append(result, EqualStep)
+		case AddedStep:
+			bRun = append(bRun, AddedStep)
+		case RemovedStep:
+			aRun = append(aRun, RemovedStep)
 		}
 	}
 	result = append(result, aRun...)
@@ -56,42 +95,42 @@ func Diff(a, b []string) []string {
 // levenshteinDistance gets the levenshtein distance and calculates the path.
 // The path is the simplest difference between the two sets.
 // See https://en.wikipedia.org/wiki/Levenshtein_distance
-func levenshteinDistance(a, b []string, aIndex, bIndex int) (int, []stepType) {
+func levenshteinDistance(comp Comparable, aIndex, bIndex int) (int, []StepType) {
 	// base case, empty sets
 	if aIndex <= 0 {
-		minPath := []stepType{}
+		minPath := []StepType{}
 		for i := bIndex; i > 0; i-- {
-			minPath = append(minPath, addedStep)
+			minPath = append(minPath, AddedStep)
 		}
 		return bIndex, minPath
 	}
 	if bIndex <= 0 {
-		minPath := []stepType{}
+		minPath := []StepType{}
 		for i := aIndex; i > 0; i-- {
-			minPath = append(minPath, removedStep)
+			minPath = append(minPath, RemovedStep)
 		}
 		return aIndex, minPath
 	}
 
 	// get the minimum of entry skip entry from a, skip entry from b, and skip entry from both
-	skipA, aPath := levenshteinDistance(a, b, aIndex-1, bIndex)
-	skipB, bPath := levenshteinDistance(a, b, aIndex, bIndex-1)
-	skipC, cPath := levenshteinDistance(a, b, aIndex-1, bIndex-1)
+	skipA, aPath := levenshteinDistance(comp, aIndex-1, bIndex)
+	skipB, bPath := levenshteinDistance(comp, aIndex, bIndex-1)
+	skipC, cPath := levenshteinDistance(comp, aIndex-1, bIndex-1)
 
 	// calculate the minimum path and add costs
 	skipA++
 	skipB++
-	cStep := equalStep
-	if a[aIndex-1] != b[bIndex-1] {
+	cSteps := []StepType{EqualStep}
+	if !comp.Equals(aIndex-1, bIndex-1) {
 		skipC++
-		cStep = diffStep
+		cSteps = []StepType{RemovedStep, AddedStep}
 	}
-	skipMin, minPath, minStep := skipA, aPath, removedStep
+	skipMin, minPath, minSteps := skipA, aPath, []StepType{RemovedStep}
 	if skipB < skipMin {
-		skipMin, minPath, minStep = skipB, bPath, addedStep
+		skipMin, minPath, minSteps = skipB, bPath, []StepType{AddedStep}
 	}
 	if skipC < skipMin {
-		skipMin, minPath, minStep = skipC, cPath, cStep
+		skipMin, minPath, minSteps = skipC, cPath, cSteps
 	}
-	return skipMin, append(minPath, minStep)
+	return skipMin, append(minPath, minSteps...)
 }
--- diff_test.go
+++ diff_test.go
@@ -7,39 +7,39 @@ import (
 
 func TestLevenshteinDistance(t *testing.T) {
 	checkLP(t, "A", "A", "0")
-	checkLP(t, "A", "B", "1")
-	checkLP(t, "A", "AB", "02")
-	checkLP(t, "A", "BA", "20")
-	checkLP(t, "AB", "A", "03")
-	checkLP(t, "BA", "A", "30")
-	checkLP(t, "kitten", "sitting", "1000102")
-	checkLP(t, "saturday", "sunday", "03301000")
-	checkLP(t, "satxrday", "sunday", "01133000")
-	checkLP(t, "ABC", "ADB", "0203")
+	checkLP(t, "A", "B", "21")
+	checkLP(t, "A", "AB", "01")
+	checkLP(t, "A", "BA", "10")
+	checkLP(t, "AB", "A", "02")
+	checkLP(t, "BA", "A", "20")
+	checkLP(t, "kitten", "sitting", "210002101")
+	checkLP(t, "saturday", "sunday", "022021000")
+	checkLP(t, "satxrday", "sunday", "0222211000")
+	checkLP(t, "ABC", "ADB", "0102")
 }
 
 func TestPartDiff(t *testing.T) {
-	checkDiff(t,
+	checkDiff(t, ",",
 		"cat,dog,pig",
 		"cat,horse,dog",
 		" cat,+horse, dog,-pig")
-	checkDiff(t,
+	checkDiff(t, ",",
 		"mike,ted,mark,jim",
 		"ted,mark,bob,bill",
 		"-mike, ted, mark,-jim,+bob,+bill")
-	checkDiff(t,
+	checkDiff(t, ",",
 		"k,i,t,t,e,n",
 		"s,i,t,t,i,n,g",
 		"-k,+s, i, t, t,-e,+i, n,+g")
-	checkDiff(t,
+	checkDiff(t, ",",
 		"s,a,t,u,r,d,a,y",
 		"s,u,n,d,a,y",
 		" s,-a,-t, u,-r,+n, d, a, y")
-	checkDiff(t,
+	checkDiff(t, ",",
 		"s,a,t,x,r,d,a,y",
 		"s,u,n,d,a,y",
 		" s,-a,-t,-x,-r,+u,+n, d, a, y")
-	checkDiff(t,
+	checkDiff(t, ",",
 		"func A() int,{,return 10,},,func C() int,{,return 12,}",
 		"func A() int,{,return 10,},,func B() int,{,return 11,},,func C() int,{,return 12,}",
 		" func A() int, {, return 10, }, ,+func B() int,+{,+return 11,+},+, func C() int, {, return 12, }")
@@ -57,14 +57,14 @@ func checkLP(t *testing.T, a, b, exp string) {
 		bParts = append(bParts, string([]rune{part}))
 	}
 
-	_, path := levenshteinDistance(aParts, bParts, len(aParts), len(bParts))
+	path := Path(&stringSliceComparable{a: aParts, b: bParts})
 	result := ""
 	for _, step := range path {
 		result += fmt.Sprint(step)
 	}
 
 	if exp != result {
-		t.Fatal("Levenshtein Distance returned unexpected result:",
+		t.Error("Levenshtein Distance returned unexpected result:",
 			"\n   Input A:  ", a,
 			"\n   Input B:  ", b,
 			"\n   Expected: ", exp,
@@ -73,10 +73,10 @@ func checkLP(t *testing.T, a, b, exp string) {
 }
 
 // checkDiff gets the labelled differences
-func checkDiff(t *testing.T, a, b, exp string) {
-	result := PartDiff(a, b, ",")
+func checkDiff(t *testing.T, sep, a, b, exp string) {
+	result := Strings(a, b, sep)
 	if exp != result {
-		t.Fatal("PartDiff returned unexpected result:",
+		t.Error("PartDiff returned unexpected result:",
 			"\n   Input A:  ", a,
 			"\n   Input B:  ", b,
 			"\n   Expected: ", exp,
