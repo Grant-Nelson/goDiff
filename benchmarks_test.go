fea20be4a86b90a24cf8da5b549dc875563f284a
Grant Nelson <grantnelson1@gmail.com>
2021-02-28 15:30:04 -0700

    Working on tests

---
+++ benchmarks_test.go
@@ -0,0 +1,101 @@
+package godiff
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Grant-Nelson/goDiff/comparable"
+)
+
+const (
+	billNyeA = `The most serious problem facing humankind is climate change. ` +
+		`All of these people breathing and burning our atmosphere has led to an ` +
+		`extraordinarily dangerous situation. I hope next generation will emerge ` +
+		`and produce technology, regulations, and a worldview that enable as many ` +
+		`of us as possible to live happy healthy lives.`
+
+	billNyeB = `The meaning of life is pretty clear: Living things strive to ` +
+		`pass their genes into the future. The claim that we would not have ` +
+		`morals or ethics without religion is extraordinary. Animals in nature ` +
+		`seem to behave in moral ways without organized religion.`
+)
+
+func runBenchmarks(b *testing.B, comp comparable.Comparable, suffix string) {
+	b.Run(fmt.Sprintf(`Hirschberg-NoReduce%s`, suffix),
+		func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				hirschbergDiff(-1, false)(comp)
+			}
+		})
+
+	b.Run(fmt.Sprintf(`Hirschberg-UseReduce%s`, suffix),
+		func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				hirschbergDiff(-1, true)(comp)
+			}
+		})
+
+	b.Run(fmt.Sprintf(`Wagner%s`, suffix),
+		func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				wagnerDiff(-1)(comp)
+			}
+		})
+
+	b.Run(fmt.Sprintf(`Hybrid-NoReduce-100%s`, suffix),
+		func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				hybridDiff(-1, false, 100)(comp)
+			}
+		})
+
+	b.Run(fmt.Sprintf(`Hybrid-UesReduce-100%s`, suffix),
+		func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				hybridDiff(-1, true, 100)(comp)
+			}
+		})
+
+	b.Run(fmt.Sprintf(`Hybrid-NoReduce-500%s`, suffix),
+		func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				hybridDiff(-1, false, 500)(comp)
+			}
+		})
+
+	b.Run(fmt.Sprintf(`Hybrid-UseReduce-500%s`, suffix),
+		func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				hybridDiff(-1, true, 500)(comp)
+			}
+		})
+}
+
+func Benchmark_Simple_Comparison(b *testing.B) {
+	comp := comparable.NewString(exampleA, exampleB)
+	runBenchmarks(b, comp, ``)
+}
+
+func Benchmark_Basic_Comparison(b *testing.B) {
+	const groups = 16
+	for i := 0; i < groups; i++ {
+		inputA := billNyeA[:len(billNyeA)*(i+1)/groups]
+		inputB := billNyeB[:len(billNyeB)*(i+1)/groups]
+		comp := comparable.NewChar(inputA, inputB)
+		suffix := `-` + fmt.Sprint(len(inputA)*len(inputB))
+		runBenchmarks(b, comp, suffix)
+	}
+}
+
+func Benchmark_Variant_Comparison(b *testing.B) {
+	const groups = 4
+	for i := 0; i < groups; i++ {
+		inputA := billNyeA[:len(billNyeA)*(i+1)/groups]
+		for j := 0; j < groups; j++ {
+			inputB := billNyeB[:len(billNyeB)*(j+1)/groups]
+			comp := comparable.NewChar(inputA, inputB)
+			suffix := `-` + fmt.Sprint(len(inputA)*len(inputB))
+			runBenchmarks(b, comp, suffix)
+		}
+	}
+}
---
+++ plusMinus_test.go
@@ -0,0 +1,43 @@
+package godiff
+
+import "testing"
+
+func Test_PlusMinus_Lines(t *testing.T) {
+	checkSlices(t, PlusMinus(exampleA, exampleB), lines(
+		`+This is an important`,
+		`+notice! It should`,
+		`+therefore be located at`,
+		`+the beginning of this`,
+		`+document!`,
+		`+`,
+		` This part of the`,
+		` document has stayed the`,
+		` same from version to`,
+		` version.  It shouldn't`,
+		` be shown if it doesn't`,
+		` change.  Otherwise, that`,
+		` would not be helping to`,
+		`-compress the size of the`,
+		`-changes.`,
+		`+compress anything.`,
+		` `,
+		`-This paragraph contains`,
+		`-text that is outdated.`,
+		`-It will be deleted in the`,
+		`-near future.`,
+		`-`,
+		` It is important to spell`,
+		`-check this dokument. On`,
+		`+check this document. On`,
+		` the other hand, a`,
+		` misspelled word isn't`,
+		` the end of the world.`,
+		` Nothing in the rest of`,
+		` this paragraph needs to`,
+		` be changed. Things can`,
+		` be added after it.`,
+		`+`,
+		`+This paragraph contains`,
+		`+important new additions`,
+		`+to this document.`))
+}
